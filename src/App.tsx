c5d240f06b680001fc97c5072fdb9c140bd8e3c4
Jerry Wang <jerryappleid761208@gmail.com>
2021-11-05 20:42:41 -0700

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default function App(): JSX.Element {
+  return (
+    <React.StrictMode>
+      <div className="flex justify-center items-center bg-gray-100 h-screen">
+        <p className="text-lg text-center font-semibold">
+          Hello World Tailwindcss!
+        </p>
+      </div>
+    </React.StrictMode>
+  );
+}
